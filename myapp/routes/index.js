d696b78ace21deb2a6cfed022262f2affb291d32
Karl <karl.l1@hotmail.com>
2017-03-11 10:48:17 +1100

    Added dates view

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,13 +5,10 @@ var cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  request('https://www.gumtree.com.au/s-karl/k0?fromSearchBox=true', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    res.send(body);
-  }
+  res.render('index', { title: 'Index' });
 });
   
   //res.render('index', { title: 'Express' });
-});
+
 
 module.exports = router;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
-  res.render('users', { title: 'Users' });
+  request('https://www.gumtree.com.au/s-karl/k0?fromSearchBox=true', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    //res.send(body);
+    var $ = cheerio.load(body);
+    var date = $('.ad-listing__date').text();
+    console.log(date)
+    res.render('users', { title: 'Users', date: date });
+  }
+}); 
 });
-
+ 
 module.exports = router;
  //res.send('respond with a resource');
