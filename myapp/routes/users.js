d813539122ff2541d57e1bda80bdc35529d31ff9
Karl <karl.l1@hotmail.com>
2017-03-11 00:50:44 +1100

    Second commit

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+  res.render('users', { title: 'Users' });
 });
 
 module.exports = router;
+ //res.send('respond with a resource');
