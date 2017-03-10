bcaf7570510f5562825c06760aaa2c2d0168a477
jack-skelton <jack.s1995@live.com.au>
2017-03-11 01:59:32 +1100

    sample scraper

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request('https://www.gumtree.com.au/s-karl/k0?fromSearchBox=true', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    res.send(body);
+  }
+});
+  
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/', function(req, res, next) {
+  
   res.render('users', { title: 'Users' });
 });
 
